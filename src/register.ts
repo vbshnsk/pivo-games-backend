83a92a78563f682a526d7c87737c60acc6bf271a
vbshnsk <vlad.bushinskiy@gmail.com>
2020-11-23 10:43:40 +0200

    fix: CORS headers

--- src/register.ts
+++ src/register.ts
@@ -3,12 +3,18 @@ import fp from 'fastify-plugin';
 import routes from './routes';
 import services from './services';
 import plugins from  './plugins';
+import fastifyCors from 'fastify-cors';
 
 const plugin = async (fastify: FastifyInstance, opts: FastifyPluginOptions) => {
     fastify
         .register(plugins)
         .register(services)
-        .register(routes);
+        .register(routes)
+        .register(fastifyCors, {
+            origin: '*',
+            methods: 'GET,POST,PUT,DELETE,OPTIONS',
+            allowedHeaders: 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+        });
 };
 
 export default fp(plugin);
