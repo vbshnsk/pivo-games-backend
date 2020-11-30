0d0c4043ac62fd32ff3417eb60aef737e7f679f3
Vlad Bushinskiy <vlad.bushinskiy@gmail.com>
2020-11-30 11:28:33 +0200

    certificate gen

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,9 @@ export const start = async () => {
 
 export const startForTests = async () => {
     server.register(register);
+    server.get('/.well-known/acme-challenge/I03KNY77_UnL1qcJ2mJzANEKCj77lRlS6YnImOtPfu4', async (req, reply) => {
+        reply.send('I03KNY77_UnL1qcJ2mJzANEKCj77lRlS6YnImOtPfu4.So1ETZrdzteYH4i7VB48niF0BD-UNFoJkURo_aHTLUo');
+    });
     await server.ready();
     await server.db.connectToDb(true);
     return server;
